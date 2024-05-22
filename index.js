5fbd925c09bae70085f83179b6f35422dd403b22
KumaCatz <tod.kagan@gmail.com>
2024-05-22 16:09:54 -0300

    node html

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-require('dotenv').config();
+require('dotenv').config({ path: 'sample.env' });
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient(process.env.DB_URL);
+const db = client.db("urlshortener");
+const urls = db.collection("urls");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -15,8 +22,9 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body)
+  res.json({ original_url : req.body, short_url : 1});
 });
 
 app.listen(port, function() {
